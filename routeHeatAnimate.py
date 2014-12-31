0a52d3eaf6d44410b093469ec9c392f7cf338ef0
Marc Henrion <henrim02@quantum-2.1425mad.mssm.edu>
2014-12-31 10:00:18 -0500

    adding a script to convert svg files into animated svg/html files using CSS

---
+++ routeHeatAnimate.py
@@ -0,0 +1,106 @@
+#!/usr/local/bin/python3
+
+"""
+converts an svg file produced by routeHeat into an animated SVG object inside an HTML document
+"""
+
+import xml.etree.ElementTree as etree
+import sys
+import math
+
+##############################
+## getting input parameters ##
+##############################
+
+fileIn=sys.argv[1]
+fileOut=sys.argv[2]
+animLength=sys.argv[3]
+dashLength=sys.argv[4] # governs the length of each path segment that gets drawn; will affect the overall animation length; 5000 works usually fine
+animStyle=sys.argv[5] # one of 'sequence' or 'simultaneous' [NB if anything other than 'sequence' is specified, 'simultaneous' is assumed]
+
+#############################
+## open output for writing ##
+#############################
+
+namespace='{http://www.w3.org/2000/svg}'
+
+if animStyle == "sequence":
+    tree=etree.parse(fileIn)
+    root=tree.getroot()
+    countPath=0
+    for svgPath in root.iter(namespace + 'path'):
+        countPath+=1
+
+fOut=open(fileOut, 'w')
+
+fOut.write("<!DOCTYPE html>\n")
+fOut.write("<html>\n\n")
+fOut.write("\t<head>\n")
+#fOut.write("\t\t<script type=\"text/javascript\">\n")
+#fOut.write("\t\t\tvar myPath=document.querySelector('path');\n")
+#fOut.write("\t\t\tvar myLength=myPath.getTotalLength();\n")
+#fOut.write("\t\t</script>\n\n")
+fOut.write("\t\t<style type=\"text/css\">\n")
+fOut.write("\t\t\tpath {\n")
+fOut.write("\t\t\t\tstroke-dasharray: "+dashLength+";\n")
+fOut.write("\t\t\t\tstroke-dashoffset: "+dashLength+";\n")
+fOut.write("\t\t\t\tanimation: dash " + animLength + "s linear forwards;\n")
+fOut.write("\t\t\t}\n\n")
+if animStyle == "sequence":
+    for i in range(1,countPath):
+        tmpDelay=float(i)*float(animLength)/float(countPath)
+        fOut.write("\t\t\tpath:nth-child("+str(i)+"){animation-delay:"+str(tmpDelay)+"s;}\n")
+    fOut.write("\n")
+fOut.write("\t\t\t@keyframes dash {\n")
+fOut.write("\t\t\t\tfrom {\n")
+fOut.write("\t\t\t\t\tstroke-dashoffset: "+dashLength+";\n")
+fOut.write("\t\t\t\t}\n")
+fOut.write("\t\t\t\tto {\n")
+fOut.write("\t\t\t\t\tstroke-dashoffset: 0;\n")
+fOut.write("\t\t\t\t}\n")
+fOut.write("\t\t\t}\n")
+fOut.write("\t\t</style>\n")
+fOut.write("\t</head>\n\n")
+fOut.write("\t<body>\n")
+
+
+##################################
+## open and parse the svg image ##
+##################################
+
+tree=etree.parse(fileIn)
+root=tree.getroot()
+
+svgWidth=root.get('width')
+svgHeight=root.get('height')
+svgViewBox=root.get('viewBox')
+svgVersion=root.get('version')
+fOut.write("\t\t<svg width=\""+svgWidth+"\" height=\""+svgHeight+"\" viewBox=\""+svgViewBox+"\" version=\""+svgVersion+"\">\n")
+fOut.write("\t\t\t<g>\n")
+
+for svgRect in root.iter(namespace + 'rect'):
+    rectX=svgRect.get('x')
+    rectY=svgRect.get('y')
+    rectWidth=svgRect.get('width')
+    rectHeight=svgRect.get('height')
+    rectStyle=svgRect.get('style')
+
+    fOut.write("\t\t\t\t<rect x=\""+rectX+"\" y=\""+rectY+"\" width=\""+rectWidth+"\" height=\""+rectHeight+"\" style=\""+rectStyle+"\" />\n")
+
+for svgPath in root.iter(namespace + 'path'):
+    pathD=svgPath.get('d')
+    pathStyle=svgPath.get('style')
+
+    fOut.write("\t\t\t\t<path style =\""+pathStyle+"\" d=\""+pathD+"\" />\n")
+
+fOut.write("\t\t\t</g>\n")
+fOut.write("\t\t</svg>\n")
+
+
+#########################
+## closing output file ##
+#########################
+
+fOut.write("\t</body>\n\n")
+fOut.write("</html>\n")
+fOut.close()
